84086a3723ed4c60d81146b6fe63f9d4e3b6f5ec
zhuhaihandachui <2512376800@qq.com>
2018-10-25 01:56:37 +0800

    update

--- src/pages/Msite/Msite.vue
+++ src/pages/Msite/Msite.vue
@@ -324,82 +324,86 @@
 	@import "../../common/stylus/mixins.styl"
 	.msite  //首页
 		width 100%
-        .msite_header
-            background-color #02a774
-            position fixed
-            z-index 100
-            left 0
-            top 0
-            width 100%
-            height 45px
-            .header_search
-	            position absolute
-	            left 15px
-	            top 50%
-	            transform translateY(-50%)
-	            width 10%
-	            height 50%
-	            .icon-sousuo
-	                font-size 25px
-	                color #fff
+		.msite_header
+			width 100%
+			background-color #02a774
+			position fixed
+			z-index 100
+			left 0
+			top 0
+			height 45px
+			.header_search
+				position absolute
+				left 15px
+				top 50%
+				transform translateY(-50%)
+				width 10%
+				height 50%
+				.icon-sousuo
+					font-size 25px
+					color #fff
 			.header_title
 				position absolute
-	            top 50%
-	            left 50%
-	            transform translate(-50%, -50%)
-	            width 50%
-	            color #fff
-	            text-align center
-	                .header_title_text
-		                font-size 20px
-		                color #fff
-		                display block
-            .header_login
-		            font-size 14px
-		            color #fff
-		            position absolute
-		            right 15px
-		            top 50%
-		            transform translateY(-50%)
-		            .header_login_text
-						color #fff
-		.msite_nav
-		bottom-border-1px(#e4e4e4)
-		margin-top 45px
-		height 200px
-		background #fff
-		.swiper-container
-			width 100%
-			height 100%
-           .swiper-wrapper
-	            width 100%
-	            height 100%
-	        	.swiper-slide
-					display flex
-					justify-content center
-					align-items flex-start
-					flex-wrap wrap
-             		.link_to_food
-             			width 25%
-              			.food_container
-							display block
-							width 100%
-							text-align center
-							padding-bottom 10px
-							font-size 0
-                  			img
-			                    display inline-block
-			                    width 50px
-			                    height 50px
-               					span
-									display block
-									width 100%
-									text-align center
-									font-size 13px
-									color #666
-            .swiper-pagination
-          		>span.swiper-pagination-bullet-active
-            		background #02a774
+				top 50%
+				left 50%
+				transform translate(-50%, -50%)
+				width 50%
+				color #fff
+				text-align center
+				.header_title_text
+					font-size 20px
+					color #fff
+					display block
+			.header_login
+				font-size 14px
+				color #fff
+				position absolute
+				right 15px
+				top 50%
+				transform translateY(-50%)
+				.header_login_text
+					color #fff
+</style>
+
+
+
+		// .msite_nav
+		// bottom-border-1px(#e4e4e4)
+		// margin-top 45px
+		// height 200px
+		// background #fff
+		// .swiper-container
+		// 	width 100%
+		// 	height 100%
+  //          .swiper-wrapper
+	 //            width 100%
+	 //            height 100%
+	 //        	.swiper-slide
+		// 			display flex
+		// 			justify-content center
+		// 			align-items flex-start
+		// 			flex-wrap wrap
+  //            		.link_to_food
+  //            			width 25%
+  //             			.food_container
+		// 					display block
+		// 					width 100%
+		// 					text-align center
+		// 					padding-bottom 10px
+		// 					font-size 0
+  //                 			img
+		// 	                    display inline-block
+		// 	                    width 50px
+		// 	                    height 50px
+  //              					span
+		// 							display block
+		// 							width 100%
+		// 							text-align center
+		// 							font-size 13px
+		// 							color #666
+  //           .swiper-pagination
+  //         		>span.swiper-pagination-bullet-active
+  //           		background #02a774
     // .msite_shop_list
     // 	top-border-1px(#e4e4e4)
     //         margin-top 10px
@@ -563,4 +567,3 @@
 			 //                        color #666
 		  //                       .segmentation
 		  //                       	color #ccc
-	</style>
